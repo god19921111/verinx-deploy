abc2b0a5d425c5bdb3c6bb11da80056fbce11786
god19921111 <130466868+god19921111@users.noreply.github.com>
2026-08-01 15:49:55 +0800

    Add frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,12 @@ import { FullPracticePage } from './pages/FullPracticePage'
 import { HistoryPage } from './pages/HistoryPage'
 import { ReportPage } from './pages/ReportPage'
 import { MembershipPage } from './pages/MembershipPage'
+import { AnalysisPage } from './pages/AnalysisPage'
+import { WeaknessPage } from './pages/WeaknessPage'
+import { ProgressPage } from './pages/ProgressPage'
+import { CheckinPage } from './pages/CheckinPage'
+import { ChallengePage } from './pages/ChallengePage'
+import { AchievementsPage } from './pages/AchievementsPage'
 
 function App() {
   return (
@@ -26,6 +32,12 @@ function App() {
           <Route path="/history" element={<HistoryPage />} />
           <Route path="/report/:id" element={<ReportPage />} />
           <Route path="/membership" element={<MembershipPage />} />
+          <Route path="/analysis" element={<AnalysisPage />} />
+          <Route path="/weakness" element={<WeaknessPage />} />
+          <Route path="/progress" element={<ProgressPage />} />
+          <Route path="/checkin" element={<CheckinPage />} />
+          <Route path="/challenge" element={<ChallengePage />} />
+          <Route path="/achievements" element={<AchievementsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
